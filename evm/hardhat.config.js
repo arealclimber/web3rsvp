7215d5a816792fd4b60bb4cdf9bed5d853623ba0
arealclimber <luffulgence@gmail.com>
2022-08-23 01:37:40 +0800

    deploy and verify the contract

--- evm/hardhat.config.js
+++ evm/hardhat.config.js
@@ -1,6 +1,38 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const { INFURA_URL, PRIVATE_KEY, RINKEBY_URL, MUMBAI_API_KEY, RINKEBY_API_KEY } =
+	process.env;
+
 module.exports = {
-  solidity: "0.8.9",
+	solidity: {
+		version: '0.8.9',
+		settings: { optimizer: { enabled: true, runs: 200 } },
+	},
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+		localhost: {
+			url: 'http://127.0.0.1:8545',
+		},
+		mumbai: {
+			url: INFURA_URL,
+			accounts: [`0x${PRIVATE_KEY}`],
+			gas: 2100000,
+			gasPrice: 8000000000,
+		},
+		rinkeby: {
+			url: RINKEBY_URL,
+			accounts: [`0x${PRIVATE_KEY}`],
+		},
+	},
+	etherscan: {
+		apiKey: {
+			polygonMumbai: MUMBAI_API_KEY,
+			rinkeby: RINKEBY_API_KEY,
+		},
+	},
 };
